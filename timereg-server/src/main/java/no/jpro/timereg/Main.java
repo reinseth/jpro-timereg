4e716767b01368524b04659fc78f9997e9568cb5
runar <runar@bell.as>
2013-06-21 11:39:50 +0200

    html static files

--- timereg-server/src/main/java/no/jpro/timereg/Main.java
+++ timereg-server/src/main/java/no/jpro/timereg/Main.java
@@ -18,7 +18,7 @@ public class Main {
         final Map<String, String> initParams = new HashMap<String, String>();
 
         initParams.put("com.sun.jersey.config.property.packages", 
-                "no.jpro");
+                "no.jpro.timereg");
 
         System.out.println("Starting grizzly...");
         SelectorThread threadSelector = GrizzlyWebContainerFactory.create(BASE_URI, initParams);     
